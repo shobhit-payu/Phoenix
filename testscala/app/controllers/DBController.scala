f1f1437b43bd8278826a40f32f0fc509a3f089f6
root <root@nmlpayupp102.idc.payugur.com>
2017-11-08 15:44:15 +0530

    mysql

--- testscala/app/controllers/DBController.scala
+++ testscala/app/controllers/DBController.scala
@@ -19,12 +19,18 @@ class DBController extends Controller {
   }
 
   def dbconnection() =  {
-    val driver = "org.voltdb.jdbc.Driver"
-    val url: String = "jdbc:voltdb://localhost:21212"
-    val val1 = ""
-    val val2 = ""
-    Class.forName(driver)
-    var conn: Connection = DriverManager.getConnection(url)
+      val driver = "com.mysql.jdbc.Driver"
+      val url: String = "jdbc:mysql://localhost:3306/payu"
+      //val driver = "org.voltdb.jdbc.Driver"
+      //val url: String = "jdbc:voltdb://localhost:21212"
+     // val driver = "org.h2.Driver"
+     // val url: String = "jdbc:h2:tcp://localhost/~/country"
+      //val url: String = "jdbc:h2:mem:test"
+
+      val val1 = ""
+      val val2 = ""
+      Class.forName(driver)
+      var conn: Connection = DriverManager.getConnection(url,"root","Password@123")
 
 
     for (i <- 1 to 10) {
@@ -49,12 +55,14 @@ class DBController extends Controller {
     var statement = conn.createStatement()
     val selectQuery = "SELECT * FROM towns"
     val rs = statement.executeQuery(selectQuery)
-    while (rs.next) {
+    conn.close()    
+/* while (rs.next) {
       val city = rs.getString("town")
       val pin = rs.getString("pin")
       val country = rs.getString("country")
       //println("city = %s, pin = %s".format(city,country))
     }
+*/
   }
 
 
