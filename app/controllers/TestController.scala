fd6d9efdafd36504fdcfc29099dc69d994a450ab
Vaibhav Gupta <vaibhav.gupta@payu.in>
2017-11-15 11:10:50 +0530

    remote router

--- app/controllers/TestController.scala
+++ app/controllers/TestController.scala
@@ -2,22 +2,17 @@ package controllers
 
 import java.io.File
 
-import play.api.mvc.{AbstractController, Action, ActionBuilder, ControllerComponents}
+import play.api.mvc.{AbstractController, Action, ControllerComponents}
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import javax.inject._
 
 import actors.Hello
 import actors.Hello._
-import akka.actor.Status.{Failure, Success}
-import akka.actor.{ActorRef, ActorSelection, ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.routing.FromConfig
 import com.typesafe.config.ConfigFactory
-import akka.pattern.ask
-import akka.util.Timeout
-import play.api.Logger
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 @Singleton
@@ -26,71 +21,32 @@ class TestController @Inject()(cc: ControllerComponents) extends AbstractControl
   val configFile = getClass.getClassLoader.getResource("myConfig.conf").getFile
   val config = ConfigFactory.parseFile(new File(configFile))
 
-  val system: ActorSystem = ActorSystem("mainSystem", config)
+  val system: ActorSystem = ActorSystem("remoteSystem", config)
 
-  val hello : ActorRef = system.actorOf(Props[Hello])
-  println(s"normal Actor reference is $hello")
+  //val hello : ActorRef = system.actorOf(Props[Hello])
+  //println(s"normal Actor reference is $hello")
+  //val router1: ActorRef = system.actorOf(FromConfig.props(Props[Hello]), "router1")
+  //println(s"Router1 reference is $router1")
 
-  val router1: ActorRef = system.actorOf(FromConfig.props(Props[Hello]).withDispatcher("my-dispatcher"), "router1")
-  println(s"Router1 reference is $router1")
+  //reading configuration from application.conf
 
-  //val router2: ActorRef = system.actorOf(FromConfig.props(Props[Hello]), "router2")
+  val remoteRouter: ActorRef = system.actorOf(FromConfig.props(Props[Hello]).withDispatcher("my-dispatcher"), "remoteRouter")
+  println(s"remoteRouter reference is $remoteRouter")
 
-  //remote router
-  val router2: ActorSelection = system.actorSelection("akka.tcp://remoteSystem@127.0.0.1:9003/user/remoteRouter")
-  println(s"Router2 reference is $router2")
-
-  def normal = Action.async {
-    //hello ! hi
-
-    implicit val timeout = Timeout(5 seconds)
-
-    val f = (hello ? futureHi)
-    val r = scala.util.Random
-    val x = r.nextInt
-    f.map(msg => {
-      Ok(s"Things are working $f")
-    })
-    /*
-    f.onComplete  {
-      case Success(value) => Ok("Normal is working now")
-      case Failure(e) => Ok("Normal is not working")
-    }
-    */
+  /*def normal = Action {
+    hello ! hi
+    Ok("Normal is working now")
   }
 
-//  def localrouter = Action {
-//    router1 ! hi
-//    Ok("Local router is working now")
-//  }
-
-  def localrouter = Action.async {
-    //hello ! hi
-
-    implicit val timeout = Timeout(5 seconds)
-
-    val f = (router1 ? futureHi)
-    val r = scala.util.Random
-    val x = r.nextInt
-    f.map(msg => {
-      //println(s"$f")
-      Ok(s"Things are working $f")
-
-    })
+  def localrouter = Action {
+    router1 ! hi
+    Ok("Local router is working now")
   }
-
-
-  def remoterouter = Action.async {
-
-    implicit val timeout = Timeout(5 seconds)
-
-    val f = (router2 ? futureHi)
-    val r = scala.util.Random
-    val x = r.nextInt
-    f.map(msg => {
-      Ok(s"Things are working $f")
-    })
-
+*/
+  def remoterouter = Action {
+    remoteRouter ! hi
+    Ok("Remote router is working now")
   }
 
 }
+
